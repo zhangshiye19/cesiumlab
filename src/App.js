4edb839918110f1f40ae60e1113530c04f139da6
张世烨 <zhangshiye19@outlook.com>
2023-03-05 10:27:49 +0800

    初次提交

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import * as Cesium from 'cesium'
+import { useEffect } from 'react';
+import './Widgets/widgets.css'
+import CMap from './CMap/CMap';
 
 function App() {
+
+  useEffect(()=>{
+    const now = CMap.getInstance().clock.currentTime;
+    const position = new Cesium.SampledPositionProperty();
+    position.addSample(now,Cesium.Cartesian3.fromDegrees(114,22,3000));
+    const next = Cesium.JulianDate.addSeconds(now,10,new Cesium.JulianDate());
+    position.addSample(next,Cesium.Cartesian3.fromDegrees(115,24,3000));
+
+    const start = now;
+    const stop = Cesium.JulianDate.addSeconds(now,100,new Cesium.JulianDate());
+    const timeInterval = new Cesium.TimeIntervalCollection([ new Cesium.TimeInterval({ start: start, stop: stop }) ]);
+    const entity = CMap.getInstance().entities.add({
+      availability: timeInterval,
+      name: 'Cesium_Air',
+      position: position,
+      model: {
+        uri: './Cesium_Air.glb',
+      }
+    })
+    CMap.getInstance().trackedEntity = entity;
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='cesiumContainer' style={{
+      height: '100%'
+    }}>
     </div>
   );
 }
